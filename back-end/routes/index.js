19d5b79ac98a3b2b86c11bc8092e24cba3670b2d
Porscha07 <porscha.uga@gmail.com>
2017-07-06 10:05:55 -0400

    working version of front/back end connection

---
+++ back-end/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/getStudents', function(req, res, next) {
+  res.json({
+  	msg:"Success!"
+  })
+});
+
+module.exports = router;
